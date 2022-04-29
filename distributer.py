d427f79542107e5bec177d13069d0caa37abbfd8
kairavishah2001 <kairavishah2001@gmail.com>
2022-04-29 23:08:36 +0530

    small update in distributer

--- distributer.py
+++ distributer.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 
 def app():
-    st.title("Retailer")
-    retailer = st.text_input("Retailer ID")
+    st.title("Distributer")
+    retailer = st.text_input("Distributer ID")
 
-    distributer = st.text_input("Distributer ID")
+    distributer = st.text_input("Retailer ID")
     option = st.selectbox(
         'Distributor', ('Suresh Modi', 'Manoj Patel', 'Raj Tiwari'))
     st.write('You selected:', option)
